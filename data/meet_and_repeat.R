40c4e7af280fd2332f5c75ce11a3eec924696851
Peshko <vapeshko@ad.helsinki.fi>
2021-12-08 17:14:04 +0200

    Ex 6 DW continued

--- data/meet_and_repeat.R
+++ data/meet_and_repeat.R
@@ -1,5 +1,5 @@
 # Valeriia Peshko
-# Exercise 5: Data Wrangling 08.12.21
+# Exercise 6: Data Wrangling 08.12.21
 
 setwd("Z:/Documents/RProjects/IODS-project/data")
 
@@ -13,13 +13,21 @@ RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/maste
 
 # overview of the data
 head(BPRS)
-glimpse(BPRS) # consists of 40 rows and 11 cols
+tail(BPRS)
+glimpse(BPRS) 
+## in the wide form the number of rows equals the number of subjects
+## and bprs measurements are spread out between columns, representing particular time points
+## consists of 40 rows and 11 cols
 
 head(RATS)
-glimpse(RATS) # consists of 16 rows and 13 cols
+tail(RATS)
+glimpse(RATS) 
+## consists of 16 rows for 16 subjects, and 13 cols, 11 of which record weight measurements on a particular day
 
+# bprs means before and during last week of treatment by treatment group
 BPRS %>% group_by(treatment) %>% summarise(n = n(), mean_w0 = mean(week0), mean_w8 = mean(week8))
 
+# weight means on first and last day of measurements by groups
 RATS %>% group_by(Group) %>% summarise(n = n(), mean_WD1 = mean(WD1), mean_WD64 = mean(WD64))
 
 # transforming categorical variables
@@ -36,16 +44,30 @@ RATSL <- RATS %>% gather(key = days, value = weights, -ID, -Group)
 
 # saving weeks and days as numbers
 BPRSL <-  BPRSL %>% mutate(week = as.integer(substr(weeks,5,5)))
-head(BPRSL)
 
 RATSL <- RATSL %>% mutate(day = as.integer(substr(days, 3,3)))
-head(RATSL)                           
-
+                           
 # serious look at the data sets
-glimpse(BPRSL) # consists of 360 rows and 5 cols
 head(BPRSL)
+tail(BPRSL)
+glimpse(BPRSL) 
+## now in the long form the number of rows equals the total number of measurements taken
+## all bprs measurements are stored in column 'bprs'
+## all records of time are kept in column 'week'
+## consists of 360 rows and 5 cols
 
-BPRSL %>% group_by(treatment, week) %>% summarise(n = n(), mean_bprs = mean(bprs))
-
-glimpse(RATSL) # consists of of 176 rows and 5 cols
 head(RATSL)
+tail(RATSL)
+glimpse(RATSL)
+## all weight measurements are stored in column 'weights'
+## all records of time are kept in column 'day'
+## consists of of 176 rows = number of measurements, and 5 cols
+
+# summaries
+## in the long form it is easier to show weekly means by groups
+BPRSL %>% group_by(treatment, week) %>% summarise(num_subjects = n(), mean_bprs = mean(bprs))
+RATSL %>% group_by(Group, day) %>% summarise(num_subjects = n(), mean_weight = mean(weights))
+
+## we can also summarise all time points
+BPRSL %>% group_by(treatment) %>% summarise(num_obs = n(), mean_bprs = mean(bprs))
+RATSL %>% group_by(Group) %>% summarise(num_obs = n(), mean_weight = mean(weights))
